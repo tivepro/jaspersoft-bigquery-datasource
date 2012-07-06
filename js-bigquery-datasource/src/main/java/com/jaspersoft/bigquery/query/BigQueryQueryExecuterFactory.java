6e0011007dd58941e3cb14788aad4349953e6ffa
matt.dahlman <matt.dahlman@gmail.com>
2012-07-06 17:59:24 +0000

    Fixed JBD-98, JBD-99 (Showing internet time if connection fails, adding copyright info)

--- js-bigquery-datasource/src/main/java/com/jaspersoft/bigquery/query/BigQueryQueryExecuterFactory.java
+++ js-bigquery-datasource/src/main/java/com/jaspersoft/bigquery/query/BigQueryQueryExecuterFactory.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2005 - 2012 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com.
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License  as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero  General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.jaspersoft.bigquery.query;
 
 import java.util.Map;
@@ -9,8 +30,8 @@ import net.sf.jasperreports.engine.query.JRQueryExecuter;
 import net.sf.jasperreports.engine.query.JRQueryExecuterFactory;
 
 /**
- * Query executer factory for HBase queries. <br/>
- * This factory creates {@link BigQueryQueryExecuter}
+ * Query executer factory for BigQuery queries. <br/>
+ * This factory creates a {@link BigQueryQueryExecuter}
  * 
  * @author Eric Diaz
  * 
@@ -20,7 +41,7 @@ public class BigQueryQueryExecuterFactory implements JRQueryExecuterFactory {
 	 * Creates an {@link BigQueryQueryExecuter}.<br/>
 	 * <br/>
 	 * This method is called at fill time for reports/datasets having a query
-	 * supported by this factory (HBaseQuery).
+	 * supported by this factory (BigQueryQueryExecuter).
 	 */
 	public JRQueryExecuter createQueryExecuter(JRDataset dataset, Map<String, ? extends JRValueParameter> parameters)
 			throws JRException {
